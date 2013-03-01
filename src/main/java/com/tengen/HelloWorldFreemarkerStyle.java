000891bbf9b8a0a039e44b8058ca021904b7efa4
Ulrich VACHON <u.vachon@gmail.com>
2013-03-02 00:28:50 +0100

    Week1 - Initial commit

---
+++ src/main/java/com/tengen/HelloWorldFreemarkerStyle.java
@@ -0,0 +1,30 @@
+package com.tengen;
+
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HelloWorldFreemarkerStyle {
+
+    public static void main(String[] args) throws IOException, TemplateException {
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldFreemarkerStyle.class, "/");
+
+        Template template = configuration.getTemplate("hello.ftl");
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("name", "World!");
+
+        StringWriter writer = new StringWriter();
+
+        template.process(map, writer);
+
+        System.out.println(writer);
+    }
+}
---
+++ src/main/java/com/tengen/HelloWorldMongoDBStyle.java
@@ -0,0 +1,21 @@
+package com.tengen;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+import java.net.UnknownHostException;
+
+public class HelloWorldMongoDBStyle {
+
+    public static void main(String[] args) throws UnknownHostException {
+        MongoClient mongoClient = new MongoClient();
+
+        DB course = mongoClient.getDB("course");
+
+        DBCollection helloCollection = course.getCollection("hello");
+
+        System.out.println(helloCollection.findOne());
+
+    }
+}
